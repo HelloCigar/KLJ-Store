a5de29801c8cd5c1925eac6c60ac3239be1707e1
HelloCigar <jckarim@up.edu.ph>
2023-05-04 12:56:46 +0800

    update cart js

--- js/Cart.js
+++ js/Cart.js
@@ -1,21 +1,25 @@
 
 $(document).ready(function(){
-    let decreaseQuantity = "#quantity button:nth-child(1)";
-    let addQuantity = "#quantity button:nth-child(2)";
-    let quantity = "#quantity input";
+    let decreaseQuantity = ".quantity button:nth-child(1)";
+    let addQuantity = ".quantity button:nth-child(3)";
+    let quantity = ".quantity input";
+
+    let currentQuantity = $(quantity).val();
 
     $(decreaseQuantity).click(function(){
-        let currentQuantity = $(quantity).val();
-        if(currentQuantity > 1){
-            $(quantity).val(--currentQuantity);
+        if(currentQuantity > 0){
+            currentQuantity--;
+            $(quantity).val(currentQuantity);
         }
-    }
+    });
 
     $(addQuantity).click(function(){
-        let currentQuantity = $(quantity).val();
-        $(quantity).val(++currentQuantity);
-    }
-    
-}
+        currentQuantity++;
+        $(quantity).val(currentQuantity);
+    });
 
-)
+    $(".removeButton").click(function(){
+        $(this).parent().parent().parent().remove();
+    })
+
+})
